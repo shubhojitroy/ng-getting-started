ccaace1b9c7285d29155307b06b5135890802c0d
shubhojit <royshubhojit@gmail.com>
2017-08-19 08:26:30 +1000

    modified existing app and added new folders under the app.

--- ng-getting-started/src/app/app.module.ts
+++ ng-getting-started/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
