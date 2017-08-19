a31322363c0d22e3244547dcc663e32c65192f3b
shubhojit <royshubhojit@gmail.com>
2017-08-19 15:01:52 +1000

    updated interface of IProduct for filtering Product-list.

--- ng-getting-started/src/app/products/product-list.component.ts
+++ ng-getting-started/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
@@ -9,8 +10,19 @@ export class ProductListComponent {
     imageWidth: number = 50;
     imagemargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [
+    // listFilter: string = 'cart';
+    
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value:string) {
+        this._listFilter = value;
+        this.filteredProducts=this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -33,7 +45,21 @@ export class ProductListComponent {
         }
     ];
 
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
-    }   
+    }
+    
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
 }
